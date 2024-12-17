a8fbb754dde87ca191c120b1b6e91cbdafefdf78
Shan Thomas <shanbio92@gmail.com>
2024-12-17 11:51:16 -0500

    Update installed_packages.R

--- installed_packages.R
+++ installed_packages.R
@@ -1,9 +1,3 @@
-#store packages,periodically back up packages
-tmp = installed.packages()
-installedpackages = as.vector(tmp[is.na(tmp[,"Priority"]), 1])
-save(installedpackages, file="~/installed_packages.rda")
-
-
 #load stored package
 #download 'installed_packages.rda' for fresh/first installations.
 load("installed_packages.rda")
@@ -11,3 +5,11 @@ load("installed_packages.rda")
 for (count in 1:length(installedpackages)) {
   install.packages(installedpackages[count])
 }
+
+
+#periodically back up packages by running code below.
+tmp = installed.packages()
+installedpackages = as.vector(tmp[is.na(tmp[,"Priority"]), 1])
+save(installedpackages, file="~/installed_packages.rda")
+
+
